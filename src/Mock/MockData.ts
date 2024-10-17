aeae583bac76db92491ea386518612efd20e1945
DingDong039 <dingdong5039@gmail.com>
2024-10-17 15:03:48 +0700

    First commit

---
+++ src/Mock/MockData.ts
@@ -0,0 +1,10 @@
+import { TrashBin } from "../types/type"; 
+
+// Mock data
+export const mockTrashBins: TrashBin[] = [
+  { id: "1", location: "Park", fillLevel: 45 },
+  { id: "2", location: "Street A", fillLevel: 100 },
+  { id: "3", location: "Mall Entrance", fillLevel: 75 },
+  { id: "4", location: "School", fillLevel: 60 },
+  { id: "5", location: "Hospital", fillLevel: 25 },
+];
